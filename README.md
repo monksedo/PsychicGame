0f2eaea6e660889a412a5d898a0591113f70013c
MonkSedo <fullstackdev1828@outlook.com>
2019-10-13 00:02:12 -0700

    Updated README.md

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# PsychicGame
-JavaScript Assignment - Create Psychic Game
+# Letter Guessing - Psychic Game
+JavaScript Assignment - Create a Letters Guessing - Psychic Game
+The goal of this game is to create a game that will randomly store a secret letter in memory, the user has 9 trial to guess for a matching letter. 
+The game must keep track of user entered letters, the wins and losses score, and reset the game when user win a point or runout of trial without winning a point. 
+
 ## Run The App
 [Click here to run](https://monksedo.github.io/PsychicGame/)
+
+## Game Features
+1. The game will randomly pick a hidden letter, and the user has to guess the letter to match hidden letter from the game.
+2. The game will start with asking the user to pick a letter.
+3. The User has 9 trial to guess.
+4. The game will compare the letters and update the wins score, and losses score.
+5. The game will restart when users run out of turns
+
+## Technology Used
+HTML, CSS, JAVASCRIPT
 
 ## Project Images
 ![Product Screenshots](assets/images/letterGuessGame.png)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,42 @@
-
+// Setup a charArray to store 26 alphagets.
 // var charArr = ["a", "b", "c"]; // Testing array
 var charArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Setup this emply array to hold the random alphabet character
+// Setup this emply array to hold the random alphabet character.
 var charEntered = [];
 
-// This variable will store a random character
+// This variable will store a random character.
 var randomChar = null;
 
-// This is what we'll use to count down
+// This is what we'll use to count down.
 var trialLelf = 9;
 
-// Variables correct and incorrect counts
+// Variables correct and incorrect counts.
 var correctCnt = 0;
 var incorrectCnt = 0;
 
-// Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
-
+// Below we created three functions to trialLeftUpdate(), ramdomCharUpdate(), and charEnteredUpdate().
 var trialLelfUpdate = function () {
-
-   // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
-
-   // (i.e. guessesLeft will get displayed in HTML)
-
    document.querySelector("#trialLelf").innerHTML = trialLelf;
-
 };
 
+// Randomly sellect an array index then store the leter from the index to randomChar variable.
 var randomCharUpdate = function () {
-
-   // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
-
    randomChar = charArr[Math.floor(Math.random() * charArr.length)];
-
 };
 
+// Insert the letter from charEntered veriable to HTML location #charEntered.
 var charEnteredUpdate = function () {
-
-   // Here we take the guesses the user has tried -- then display it as letters separated by commas.
-
    document.querySelector("#charEntered").innerHTML = charEntered.join(", ");
 };
 
-// Function will be called when we reset everything
-
+// Reset the whole game to initial value.
 var resetPage = function () {
-
    trialLelf = 9;
-
    charEntered = [];
-
    trialLelfUpdate();
-
    randomCharUpdate();
-
    charEnteredUpdate();
 };
 
@@ -62,45 +44,44 @@ var resetPage = function () {
 trialLelfUpdate();
 randomCharUpdate();
 
-// This function will capture the keyboard clicks.
+// This function will capture the key pressed/released.
 document.onkeyup = function (e) {
+   trialLelf--; // Countdown.
 
-   // It's going to reduce the guesses by one
-   trialLelf--;
-
-   // Lowercase the letter
+   // convert the string to lower case.
    var char = String.fromCharCode(e.which).toLowerCase();
 
-   // Then add the guess to the guessedLetters array
+   // Store the string in the array charEntered[].
    charEntered.push(char);
 
-   // Then its going to run the update functions
+   // Call these two functions.
    trialLelfUpdate();
    charEnteredUpdate();
 
-   // We'll check if there's a match.
+   // Compare the string user entered (char) game seleted string.
    if (char === randomChar) {
 
-      // If there is then we win and we'll update the HTML to display the win.
+      // Countup winning score.
       correctCnt++;
 
       document.querySelector("#correct").innerHTML = correctCnt;
-      // Then we'll reset the game
 
+      // call page reset function.
       resetPage();
    }
 
-   // If we are out of guesses...
+   // Compare if trialLelf is equal to 0.
    if (trialLelf === 0) {
 
-      // Then we will loss and we'll update the HTML to display the loss.
+      // increase the loss count if user run out of geueses without a match.
       incorrectCnt++;
 
+      // Insert the loss count value to HTML location #incorrect.
       document.querySelector("#incorrect").innerHTML = incorrectCnt;
 
-      // Then we'll call the reset.
+      // call resetPage function
       resetPage();
 
    }
 
-};
+}; // End of code
